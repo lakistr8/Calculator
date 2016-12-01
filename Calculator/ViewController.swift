22e1bfeafda8fcdbabe1d77db69b9d27eea50b93
Lazar <lakiando@outlook.com>
2016-12-01 23:44:12 +0100

    added IBOutlets for rest of buttons and named them

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     
     @IBOutlet var digitButtons : [UIButton]!
     @IBOutlet var operatorButtons : [UIButton]!
+    @IBOutlet weak var decimalButton : UIButton!
+    @IBOutlet weak var changeSingButton : UIButton!
+    @IBOutlet weak var clearButton : UIButton!
+    @IBOutlet weak var deleteButton : UIButton!
+    
     
    
 }
