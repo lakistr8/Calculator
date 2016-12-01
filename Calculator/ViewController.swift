44f53ac0dab707ec7853ef2bd64bbd46f6f62309
Lazar <lakiando@outlook.com>
2016-12-02 00:09:23 +0100

    added new extension ViewController and added new func validate operand input

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -36,8 +36,21 @@ extension Internal {
         assingButtonTargets()
         configureDecimalButton()
     }
-    
 }
+
+extension ViewController {
+    
+    func validateOperandInput() -> Double? {
+        guard let numString = resultField.text else {
+            return nil
+        }
+        
+        let formatter = NumberFormatter()
+        let numb = formatter.number(from: numString)?.doubleValue
+        return numb
+    }
+}
+
 
 extension ViewController {
     
@@ -68,23 +81,19 @@ extension ViewController {
             sender.backgroundColor = UIColor.black.withAlphaComponent(0.2)
             return
         }
-        //	od buttona su vec pozadine providne
-        //	moramo povecati alpha deo da bi se videlo na dodiru
         
-        //	ovo je nacin da se izvuce RGBA komponente iz UIColor
-        //	setup default (black)
         var r : CGFloat = 0
         var g : CGFloat = 0
         var b : CGFloat = 0
-        //	koristimo 20% vidljivosti
+        
         var a : CGFloat = 0.2
-        //	pomocu ovog metoda cemo popuniti komponente UIColor vrednosti
+        
         guard let _ = sender.backgroundColor?.getRed(&r, green: &g, blue: &b, alpha: &a) else {
-            //	ако екстракција не успе, онда се врати на црно, као горе
+            
             sender.backgroundColor = UIColor.black.withAlphaComponent(0.2)
             return
         }
-        //	ako je uspelo koristimo alpha 2X
+        
         sender.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: a*2)
     }
     
