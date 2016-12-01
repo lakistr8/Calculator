b6250ee053f172113238eb8e3d2fcbadcd917c39
Lazar <lakiando@outlook.com>
2016-12-01 23:32:03 +0100

    created new touple Internal ho is ViewController and do extension Internal and viewDidLoad copied in there

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+   
 }
 
+typealias Internal = ViewController
+extension Internal {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
